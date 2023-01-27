bcf3b49aa67a8a426c3eb9ee60fe415f150cc0d4
Jonghyun-Park1027 <121370967+Jonghyun-Park1027@users.noreply.github.com>
2023-01-27 09:32:25 +0900

    day 7 - 재도전

--- DFS BFS/DFS_BFS_baekjun.py
+++ DFS BFS/DFS_BFS_baekjun.py
@@ -18,6 +18,7 @@ for i in graph:
         if visited[j - 1] != True:
             visited[j - 1] = True
 '''
+''' 6일차 재도전
 # 다시 도전
 n, m, v= map(int,input().split())
 graph = [[]]
@@ -38,3 +39,30 @@ visited = [False] * 5
 dfs(graph, v, visited)
 # print(visited)
 print(graph)
+'''
+n, m, v = map(int, input().split())
+graph = [[]]
+for _ in range(m):
+    graph.append(list(map(int, input().split())))
+answer = []
+
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    # print(visited.index(True))
+    print(v, end=" ")
+    for i in graph[v]:
+        if not visited[i] :
+            for j in graph:
+                if i in j:
+                    print("def call")
+                    dfs(graph, i, visited)
+
+
+
+
+
+
+visited = [False] * m
+
+dfs(graph, v, visited)
