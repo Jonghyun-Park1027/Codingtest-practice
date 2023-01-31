2c1009570b44a4beaa6f250c5c0de17c302f0576
Jonghyun-Park1027 <121370967+Jonghyun-Park1027@users.noreply.github.com>
2023-01-31 09:17:39 +0900

    day 10 -dfs solve

--- DFS BFS/DFS.py
+++ DFS BFS/DFS.py
@@ -93,33 +93,35 @@ graph = []
 for _ in range(N):
     graph.append(list(map(int, list(input()))))
 count = 0
-def dfs(graph,n,m):
+
+
+def dfs(graph, n, m):
     global N, M
     if n >= N and m >= M:
         return
     graph[n][m] = 1
 
-
-
-    #up
-    if n-1 > -1 and graph[n-1][m] == 0:
-        dfs(graph, n-1, m)
-    #down
-    if n+1 < N and graph[n+1][m] == 0:
-        dfs(graph, n+1, m)
-    #left
-    if m-1 > -1 and graph[n][m-1] == 0:
-        dfs(graph, n, m-1)
-    #right
-    if m+1 < M and graph[n][m+1] == 0:
-        dfs(graph, n, m+1)
+    # up
+    if n - 1 > -1 and graph[n - 1][m] == 0:
+        dfs(graph, n - 1, m)
+    # down
+    if n + 1 < N and graph[n + 1][m] == 0:
+        dfs(graph, n + 1, m)
+    # left
+    if m - 1 > -1 and graph[n][m - 1] == 0:
+        dfs(graph, n, m - 1)
+    # right
+    if m + 1 < M and graph[n][m + 1] == 0:
+        dfs(graph, n, m + 1)
 
     return
-#find '0'
+
+
+# find '0'
 for i in range(N):
     for j in range(M):
         if graph[i][j] == 0:
-            dfs(graph,i,j)
+            dfs(graph, i, j)
             count += 1
 
 print(count)
