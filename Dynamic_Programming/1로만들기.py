908cf01819451c757456ae5561a2b5d5e3d33d23
Jonghyun-Park1027 <121370967+Jonghyun-Park1027@users.noreply.github.com>
2023-02-17 20:03:33 +0900

    day 17 - 1 challenge(not solve)

--- Dynamic_Programming/1로만들기.py
+++ Dynamic_Programming/1로만들기.py
@@ -1,11 +1,28 @@
-d = [0] * 100
-
-def fibo(x):
-    if x== 1 or x==2:
-        return 1
-    if d[x] != 0:
-        return d[x]
-    d[x] = fibo(x-1)+ fibo(x-2)
-    return d[x]
-
-print(fibo(99), d)
+x = int(input())
+# x % 5 == 0
+count = 0
+def cal(x):
+    global count
+    if x <= 1:
+        return
+    if x % 5 ==0:
+        print(x, "5")
+        count += 1
+        cal(x/5)
+        # print("1")
+    # x %3 == 0
+    elif x % 3 ==0:
+        print(x,"3")
+        cal(x/3)
+        # print("2")
+    # x % 2 == 0
+    elif x% 2 == 0:
+        print(x, "2")
+        cal(x/2)
+    # x -1
+    if (x -1) % 5 ==0 or (x-1) % 3 == 0 or (x-1) % 2 == 0:
+        print(x, "1")
+        cal(x-1)
+# print(count)
+cal(x)
+print(count)
